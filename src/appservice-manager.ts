98d7cadf3b8a4063f7d0465ceafa09ecb10dee53
zhangchunsheng <zhangchunsheng423@gmail.com>
2020-10-19 16:38:08 +0800

    fix TypeError: matrixBridge.opts.registration.getSenderLocalpart is not a function

--- src/appservice-manager.ts
+++ src/appservice-manager.ts
@@ -52,7 +52,7 @@ export class AppserviceManager extends Manager {
     } else if (typeof registration === 'string') {
 
     } else if (registration != null && typeof registration === 'object' && 'sender_localpart' in registration) {
-      this.localpart = registration.sender_localpart
+      this.localpart = registration!.sender_localpart
     }
 
     const userBridgeStore = matrixBridge.getUserStore()
